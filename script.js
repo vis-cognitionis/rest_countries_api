8b85fd7ffa842f02f5d3377e8ee5b22f9fa8c758
Vis Cognitionis <omeralemdar94@gmail.com>
2023-03-29 18:07:57 +0300

    Refactor css styles

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ const filterArea = document.getElementById("filter-area");
 
 filterArea.addEventListener("click", () => {
   regionOptions.classList.toggle("region-options-open");
-  console.log("girdi");
+});
+
+//Required to close the options section that opens when clicking outside the filtering area.
+document.addEventListener("click", (event) => {
+  if (!event.target.closest("#filter-area")) {
+    regionOptions.classList.remove("region-options-open");
+  }
 });
