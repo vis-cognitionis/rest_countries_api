a253974c743ec8d59b199e6a49fc82d55094afd0
Vis Cognitionis <omeralemdar94@gmail.com>
2023-03-29 18:19:41 +0300

    Refactor home_page

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const regionOptions = document.querySelector(".region-options");
 const filterArea = document.getElementById("filter-area");
+const main = document.getElementById("country-cards");
 
 filterArea.addEventListener("click", () => {
   regionOptions.classList.toggle("region-options-open");
 });
 
-//Required to close the options section that opens when clicking outside the filtering area.
+//sample data
+const divArray = Array(10).fill(
+  `<div style="border: 1px solid red; width: 200px; height:300px"></div>`
+);
+main.innerHTML = divArray.join("");
+
+//Required to close the options section that opens when clicking outside the filtering area
 document.addEventListener("click", (event) => {
   if (!event.target.closest("#filter-area")) {
     regionOptions.classList.remove("region-options-open");
